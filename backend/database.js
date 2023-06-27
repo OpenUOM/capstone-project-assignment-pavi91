c57db8af1f294d844c065e47c3f336e0ee61dca1
pavimeth2 <pavimeth2@gmail.com>
2023-06-27 20:54:25 +0530

    database function changes

--- backend/database.js
+++ backend/database.js
@@ -23,7 +23,7 @@ const dbinitialize = async () => {
 }
 
 const readTeachers = async () => {
-    const sql = `SELECT * FROM dummyData`
+    const sql = `SELECT * FROM teacher`
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql)
@@ -93,7 +93,7 @@ const deleteTeacher = async (id) => {
 }
 
 const readStudents = async () => {
-    const sql = `SELECT * FROM dummyData`
+    const sql = `SELECT * FROM students`
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql)
