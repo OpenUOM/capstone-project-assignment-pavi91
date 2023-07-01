abd457921d54af3df962a9054690003d5bba4ab7
pavimeth2 <pavimeth2@gmail.com>
2023-07-02 00:44:15 +0530

    f8

--- frontend/test/student-add.test.js
+++ frontend/test/student-add.test.js
@@ -1,10 +1,33 @@
 import { Selector } from 'testcafe';
 
 fixture`Testing Student UI`
-    .page`https://devexpress.github.io/testcafe/example`;
+    .page`http://localhost:4200/addStudent`;
 
 test('Testing add students', async t => {
     
-    await t.expect(true).ok();
     
+    // Fill in the form fields and submit
+    await t.typeText("#student-id", "999999");
+    await t.typeText("#student-name", "Pasindu Basnayaka");
+    await t.typeText("#student-age", "45");
+    await t.typeText("#student-Hometown", "Catholic");
+    await t.click("#student-add");
+
+    await t.navigateTo("/student");
+
+    const table = Selector('#student-table');
+    const rowCount = await table.find('tr').count;
+    
+    // Check if the expected student name is present in any row
+    let isStudentFound = false;
+    for (let i = 0; i < rowCount; i++) {
+        const tdText = await table.find('tr').nth(i).innerText;
+        if (tdText.includes("Pasindu Basnayaka")) {
+            isStudentFound = true;
+            break;
+        }
+    }
+    
+    // Assert if the student is found
+    await t.expect(isStudentFound).ok();
 });
