18b565b2f59b4709717e9012e05b7568ad733a99
pavimeth2 <pavimeth2@gmail.com>
2023-07-02 00:29:27 +0530

    f6

--- frontend/test/dummy.test.js
+++ frontend/test/dummy.test.js
@@ -1,7 +1,12 @@
+import { Selector } from 'testcafe';
+
 fixture`Dummy Test`
     .page`https://devexpress.github.io/testcafe/example`;
 
 test('Dummy Test', async t => {
-    
-    await t.expect(true).ok();
+    // Use a selector to target the <h1> element
+    const h1Element = Selector('h1');
+
+    // Assert that the <h1> element exists on the page
+    await t.expect(h1Element.exists).ok();
 });
